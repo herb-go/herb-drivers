6b927be31e99bba91e3f27cb96ad0022d53c309f
jarlyyn <jarlyyn@qq.com>
2020-07-26 23:03:36 +0800

    update

---
+++ middleware/hiredmiddleware/hiredmiddleware.go
@@ -0,0 +1,27 @@
+package hiredmiddleware
+
+import (
+	"github.com/herb-go/herb/middleware"
+	"github.com/herb-go/herb/middleware/middlewarefactory"
+	"github.com/herb-go/worker"
+	"github.com/herb-go/herb-drivers/overseers/middlewareoverseer"
+)
+
+type Config struct {
+	ID string
+}
+
+var NewFactory = func() middlewarefactory.Factory {
+	return func(loader func(v interface{}) error) (middleware.Middleware, error) {
+		c := &Config{}
+		err := loader(c)
+		if err != nil {
+			return nil, err
+		}
+		m := middlewareoverseer.GetMiddlewareByID(c.ID)
+		if m == nil {
+			return nil, worker.ErrWorkerNotFound
+		}
+		return m, nil
+	}
+}
