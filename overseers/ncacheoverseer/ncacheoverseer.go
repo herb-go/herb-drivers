584c83034ae3e569ee51a5676edad71ad5ff60bf
jarlyyn <jarlyyn@qq.com>
2021-03-18 23:11:54 +0800

    update

--- overseers/ncacheoverseer/ncacheoverseer.go
+++ overseers/ncacheoverseer/ncacheoverseer.go
@@ -1,6 +1,9 @@
 package ncacheoverseer
 
-import "github.com/herb-go/worker"
+import (
+	"github.com/herb-go/datamodules/ncache"
+	"github.com/herb-go/worker"
+)
 
 //Config overseer config struct
 type Config struct {
@@ -9,6 +12,32 @@ type Config struct {
 //ApplyTo apply config to overseer
 func (c *Config) ApplyTo(o *worker.PlainOverseer) error {
 	o.WithIntroduction("Ncache workers")
+	o.WithOutsourceFunc(func(o *worker.Outsourced) error {
+		c := ncache.New()
+		config := ncache.NewConfig()
+		err := o.TranningPlan(config)
+		if err != nil {
+			return err
+		}
+		err = config.ApplyTo(c)
+		if err != nil {
+			return err
+		}
+		worker.Hire(o.Name, &c)
+		worker.OnStart(func() {
+			err := c.Start()
+			if err != nil {
+				panic(err)
+			}
+		})
+		worker.OnStop(func() {
+			err := c.Stop()
+			if err != nil {
+				panic(err)
+			}
+		})
+		return nil
+	})
 	return nil
 }
 
--- overseers/nestedcacheoverseer/nestedcacheoverseer.go
+++ overseers/nestedcacheoverseer/nestedcacheoverseer.go
@@ -1,12 +1,24 @@
 package nestedcacheoverseer
 
 import (
+	"github.com/herb-go/datamodules/ncache"
 	"github.com/herb-go/datamodules/ncache/builderconfig"
 	"github.com/herb-go/worker"
 )
 
 //Config overseer config struct
 type Config struct {
+}
+
+func train(c *ncache.NestedCache, loader func(v interface{}) error) error {
+	config := &builderconfig.BuildConfig{}
+	err := loader(config)
+	builders, err := config.CreateBuilders()
+	if err != nil {
+		return err
+	}
+	c.WithBuilder(builders...)
+	return nil
 }
 
 //ApplyTo apply config to overseer
@@ -22,17 +34,33 @@ func (c *Config) ApplyTo(o *worker.PlainOverseer) error {
 			if t == nil {
 				continue
 			}
-			config := &builderconfig.BuildConfig{}
-			err := t.TranningPlan(config)
+			err := train(cache, t.TranningPlan)
 			if err != nil {
 				return err
 			}
-			builders, err := config.CreateBuilders()
-			if err != nil {
-				return err
-			}
-			cache.WithBuilder(builders...)
+			return nil
 		}
+		return nil
+	})
+	o.WithOutsourceFunc(func(o *worker.Outsourced) error {
+		c := ncache.NewNestedCache()
+		err := train(c, o.TranningPlan)
+		if err != nil {
+			return err
+		}
+		worker.Hire(o.Name, &c)
+		worker.OnStart(func() {
+			err := c.Start()
+			if err != nil {
+				panic(err)
+			}
+		})
+		worker.OnStop(func() {
+			err := c.Stop()
+			if err != nil {
+				panic(err)
+			}
+		})
 		return nil
 	})
 	return nil
